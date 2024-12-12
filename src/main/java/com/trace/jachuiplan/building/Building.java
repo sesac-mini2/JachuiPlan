bb8ea09084651d31ae09e476c2fae987dcc15f5e
hwakyung <hwakyung5531@gmail.com>
2024-12-12 13:41:05 +0900

    Building, OfficeHotel Entity 생성
    - building 클래스명, 변수명 수정
    - OfficeHotel 생성

---
+++ src/main/java/com/trace/jachuiplan/building/Building.java
@@ -0,0 +1,50 @@
+package com.trace.jachuiplan.building;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Building {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "BUILDING_TYPE", nullable = false, columnDefinition = "NUMBER(1)")
+    private Integer buildingType;
+
+    @Column(name = "SGGCD", length = 5, nullable = true)
+    private String sggcd;
+
+    @Column(name = "UMDNM", length = 20, nullable = true)
+    private String umdnm;
+
+    @Column(name = "TOTAL_FLOOR_AR", nullable = true, columnDefinition = "NUMBER(8, 2)")
+	private Double totalFloorAr;
+
+    @Column(name = "FLOOR", nullable = true, columnDefinition = "NUMBER(3)")
+	private Integer floor;
+
+    @Column(name = "BUILD_YEAR", nullable = true, columnDefinition = "NUMBER(4)")
+	private Integer buildYear;
+
+    @Column(name = "DEPOSIT", nullable = true, columnDefinition = "NUMBER(10)")
+	private Long deposit;
+
+    @Column(name = "MONTHLY_RENT", nullable = true, columnDefinition = "NUMBER(10)")
+	private Long monthlyRent;
+
+    @Column(name = "DEALDATE")
+	private LocalDateTime dealdate;
+
+    @Column(name = "BUILDING_NAME", length = 300, nullable = true)
+	private String buildingName;
+
+    @Column(name = "HOUSE_TYPE", length = 30, nullable = true)
+	private String houseType;
+}
---
+++ src/main/java/com/trace/jachuiplan/officeHotel/OfficeHotel.java
@@ -0,0 +1,47 @@
+package com.trace.jachuiplan.officeHotel;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OfficeHotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "SGGCD", length = 5, nullable = true)
+    private String sggcd;
+
+    @Column(name = "UMDNM", length = 20, nullable = true)
+    private String umdnm;
+
+    @Column(name = "EXCLU_USE_AR", nullable = true, columnDefinition = "NUMBER(8, 2)")
+    private Double exclu_use_ar;
+
+    @Column(name = "FLOOR", nullable = true, columnDefinition = "NUMBER(3)")
+    private Integer floor;
+
+    @Column(name = "BUILD_YEAR", nullable = true, columnDefinition = "NUMBER(4)")
+    private Integer buildYear;
+
+    @Column(name = "DEPOSIT", nullable = true, columnDefinition = "NUMBER(10)")
+    private Long deposit;
+
+    @Column(name = "MONTHLY_RENT", nullable = true, columnDefinition = "NUMBER(10)")
+    private Long monthlyRent;
+
+    @Column(name = "DEALDATE")
+    private LocalDateTime dealdate;
+
+    @Column(name = "JIBUN", length = 10, nullable = true)
+    private String jibun;
+
+    @Column(name = "BUILDING_NAME", length = 300, nullable = true)
+    private String buildingName;
+}
