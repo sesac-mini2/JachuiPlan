9b3a0746541df1eb514bbe9b8340a2c2529d0832
Jaehyuk-Lee <alex00728@outlook.com>
2024-12-20 15:02:33 +0900

    add ExactSize Annotation

--- src/main/java/com/trace/jachuiplan/officeHotel/OfficeHotelApiController.java
+++ src/main/java/com/trace/jachuiplan/officeHotel/OfficeHotelApiController.java
@@ -1,5 +1,6 @@
 package com.trace.jachuiplan.officeHotel;
 
+import com.trace.jachuiplan.CustomAnnotation.ExactSize;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +14,17 @@ public class OfficeHotelApiController {
     private final OfficeHotelService officeHotelService;
 
     @GetMapping("/api/officeHotel/{startyearmonth}/{endyearmonth}/{sggcd}")
-    public ResponseEntity<List<OfficeHotel>> getDeals(@PathVariable("startyearmonth") @Size(max = 6, min = 6) String startyearmonth,
-                                                      @PathVariable("endyearmonth") @Size(max = 6, min = 6) String endyearmonth,
-                                                      @PathVariable("sggcd") @Size(max = 5, min = 5) String sggcd) {
-        List<OfficeHotel> officeHotelDeals = officeHotelService.getOfficeHotelDeals(startyearmonth, endyearmonth, sggcd);
-        return ResponseEntity.ok()
-                .body(officeHotelDeals);
-    }
-
-    @PostMapping("/api/officeHotel/{startyearmonth}/{endyearmonth}/{sggcd}")
-    public ResponseEntity<List<OfficeHotel>> getDealsPost(@PathVariable("startyearmonth") @Size(max = 6, min = 6) String startyearmonth,
-                                                      @PathVariable("endyearmonth") @Size(max = 6, min = 6) String endyearmonth,
-                                                      @PathVariable("sggcd") @Size(max = 5, min = 5) String sggcd) {
+    public ResponseEntity<List<OfficeHotel>> getDeals(@PathVariable("startyearmonth") @ExactSize(6) String startyearmonth,
+                                                      @PathVariable("endyearmonth") @ExactSize(6) String endyearmonth,
+                                                      @PathVariable("sggcd") @ExactSize(6) String sggcd) {
         List<OfficeHotel> officeHotelDeals = officeHotelService.getOfficeHotelDeals(startyearmonth, endyearmonth, sggcd);
         return ResponseEntity.ok()
                 .body(officeHotelDeals);
     }
 
     @GetMapping(value = "/api/officeHotel/{startyearmonth}/{endyearmonth}")
-    public ResponseEntity<List<OfficeHotel>> getDealsWithSggList(@PathVariable("startyearmonth") @Size(max = 6, min = 6) String startyearmonth,
-                                                                @PathVariable("endyearmonth") @Size(max = 6, min = 6) String endyearmonth,
+    public ResponseEntity<List<OfficeHotel>> getDealsWithSggList(@PathVariable("startyearmonth") @ExactSize(6) String startyearmonth,
+                                                                @PathVariable("endyearmonth") @ExactSize(6) String endyearmonth,
                                                                 // 일단은 sggcd부터 리스트로 받는 기능 만들기
                                                                 @RequestParam(value = "sggcds") List<@Size(max = 5, min = 5) String> sggcds) {
         List<OfficeHotel> officeHotelDeals = officeHotelService.getOfficeHotelDeals(startyearmonth, endyearmonth, sggcds);
