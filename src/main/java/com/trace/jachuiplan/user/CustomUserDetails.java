9fc3f31bb3119b753ea86b30f6ff281028124064
hwakyung <hwakyung5531@gmail.com>
2024-12-24 15:24:23 +0900

    CustomUserDetails 추가

---
+++ src/main/java/com/trace/jachuiplan/user/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.trace.jachuiplan.user;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    @Getter
+    private String nickname;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username, String password, String nickname, List<GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+}
