9855e1f9cc9095275b2ba536e79049c823187476
Jaehyuk-Lee <alex00728@outlook.com>
2024-12-30 10:35:10 +0900

    RequestMapping 추가, 필요없는 코드 제거

--- src/main/java/com/trace/jachuiplan/officeHotel/OfficeHotelApiController.java
+++ src/main/java/com/trace/jachuiplan/officeHotel/OfficeHotelApiController.java
@@ -4,7 +4,7 @@ import com.trace.jachuiplan.CustomAnnotation.ExactSize;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,29 +12,11 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/officeHotel")
 public class OfficeHotelApiController {
     private final OfficeHotelService officeHotelService;
 
-    @GetMapping("/api/officeHotel/{startYearMonth}/{endYearMonth}/{sggcd}")
-    public ResponseEntity<List<OfficeHotel>> getDeals(@PathVariable("startYearMonth") @ExactSize(6) String startYearMonth,
-                                                      @PathVariable("endYearMonth") @ExactSize(6) String endYearMonth,
-                                                      @PathVariable("sggcd") @ExactSize(6) String sggcd) {
-        List<OfficeHotel> officeHotelDeals = officeHotelService.getOfficeHotelDeals(startYearMonth, endYearMonth, sggcd);
-        return ResponseEntity.ok()
-                .body(officeHotelDeals);
-    }
-
-    @GetMapping(value = "/api/officeHotel/{startYearMonth}/{endYearMonth}")
-    public ResponseEntity<List<OfficeHotel>> getDealsWithSggList(@PathVariable("startYearMonth") @ExactSize(6) String startYearMonth,
-                                                                @PathVariable("endYearMonth") @ExactSize(6) String endYearMonth,
-                                                                // 일단은 sggcd부터 리스트로 받는 기능 만들기
-                                                                @RequestParam(value = "sggcds") List<@ExactSize(5) String> sggcds) {
-        List<OfficeHotel> officeHotelDeals = officeHotelService.getOfficeHotelDeals(startYearMonth, endYearMonth, sggcds);
-        return ResponseEntity.ok()
-                .body(officeHotelDeals);
-    }
-
-    @GetMapping("/api/officeHotel/search")
+    @GetMapping("/search")
     public ResponseEntity<List<OfficeHotel>> searchOfficeHotels(
             @RequestParam(name = "sggcds") List<@ExactSize(5) String> sggcds,
             @RequestParam(name = "startYearMonth") @ExactSize(6) String startYearMonth,
@@ -62,7 +44,7 @@ public class OfficeHotelApiController {
         return ResponseEntity.ok(officeHotels);
     }
 
-    @GetMapping("/api/officeHotel/average")
+    @GetMapping("/average")
     public ResponseEntity<List<OfficeHotelFilterDTO>> averageOfficeHotels(
             @RequestParam(name = "sggcds") List<@ExactSize(5) String> sggcds,
             @RequestParam(name = "startYearMonth") @ExactSize(6) String startYearMonth,
