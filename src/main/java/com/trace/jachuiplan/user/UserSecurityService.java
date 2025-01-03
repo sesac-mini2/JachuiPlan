f23a4afdb409bd367e6bceff8298b7c8f4081829
jjoool <geunsu1019@gmail.com>
2025-01-03 19:53:29 +0900

    OAuth2 구글 로그인

--- src/main/java/com/trace/jachuiplan/user/UserSecurityService.java
+++ src/main/java/com/trace/jachuiplan/user/UserSecurityService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
