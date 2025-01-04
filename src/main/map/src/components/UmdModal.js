f049759f8b6e6afd3bd2bc7cfd11f06dad02f874
Jaehyuk-Lee <alex00728@outlook.com>
2025-01-05 02:26:30 +0900

    update UmdGraph

--- src/main/map/src/components/UmdModal.js
+++ src/main/map/src/components/UmdModal.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ScrapButton from "./ScrapButton";
 import Scrap from './Graph/Scrap';
 import UmdGraph from './Graph/UmdGraph';
 
-const UmdModal = ({ isAuthenticated, targetUmd, isShowUmdModal, closeUmdModal }) => {
+const UmdModal = ({ isAuthenticated, targetUmd, isShowUmdModal, closeUmdModal,
+  startYearMonth,
+  endYearMonth,
+  selectedType,
+  rentType,
+  startYear,
+  endYear,
+  selectedFloor,
+  minArea,
+  maxArea
+}) => {
   const modalStyle = {
     position: "absolute",
     top: "50%", // 세로 중앙 정렬
@@ -26,19 +37,44 @@ const UmdModal = ({ isAuthenticated, targetUmd, isShowUmdModal, closeUmdModal })
     top: "1.2rem",
     right: "1.2rem",
   };
+  
+  const [region, setRegion] = useState({cd: '', name: ''});
+  
+  useEffect(() => {
+    axios.get(`/api/regioncd/id/${targetUmd}`)
+    .then((res) => {
+        setRegion({
+          cd: res.data[0].regioncd,
+          name: res.data[0].regionKoreanName
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, [targetUmd]);
 
   return (
     <>
       <div id="umdModal" className={isShowUmdModal ? "card show" : "card d-none"} style={modalStyle} tabIndex="-1" aria-hidden="false">
         <div>
           <div>
-            <span className="umdnm me-3 h4 align-middle">{targetUmd}</span>
+            <span className="umdnm me-3 h4 align-middle">{region.name}</span>
             <ScrapButton isAuthenticated={isAuthenticated} targetUmd={targetUmd}></ScrapButton>
           </div>
           <button type="button" className="btn-close" aria-label="Close" style={closeButtonStyle} onClick={closeUmdModal}></button>
         </div>
         <Scrap />
-        <UmdGraph />
+        <UmdGraph region={region} 
+          startYearMonth={startYearMonth}
+          endYearMonth={endYearMonth}
+          selectedType={selectedType}
+          rentType={rentType}
+          startYear={startYear}
+          endYear={endYear}
+          selectedFloor={selectedFloor}
+          minArea={minArea}
+          maxArea={maxArea}
+        />
       </div>
     </>
   )
